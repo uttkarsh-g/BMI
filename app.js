09b6602e2096cce638b04094c691cac90583e935
Alexhawak <hloji999@gmail.com>
2024-12-13 19:09:34 +0530

    update progress

---
+++ app.js
@@ -0,0 +1,8 @@
+const r = document.querySelector('#result');
+const w = document.querySelector('#weight');
+const h = document.querySelector('#height');
+const f = document.querySelector('form');
+
+f.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
