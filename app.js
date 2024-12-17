2c239aab6e0b51a1f4e81b7e67abacd5ef2393e3
Alexhawak <hloji999@gmail.com>
2024-12-17 22:12:46 +0530

    update

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const w = document.querySelector('#weight');
 const h = document.querySelector('#height');
 const f = document.querySelector('form');
 
+const calc = () => {
+  const heightInMeter = h.value * 0.01;
+  const weight = w.value;
+  return weight / (heightInMeter * heightInMeter);
+};
+
 f.addEventListener('submit', (e) => {
   e.preventDefault();
+  r.innerText = calc().toFixed(1);
 });
